76c9ea794f995954c44ecc7a17c1915207c6244a
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-13 00:01:00 +0900

    feat: thor追加

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 # Gemfile
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "rake"
+gem 'rake'
 gem 'rubocop', require: false
+gem 'thor', '~> 1.3', '>= 1.3.1'
--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,26 @@
-puts 'hello world!'
+require 'thor'
+require 'csv'
+require 'io/console'
+
+class TodoCLI < Thor
+  desc 'create TASK', '新しいタスクを作成'
+  TodoCLI.start(ARGV)
+  def oink
+    puts '今からタスクの計測を開始します。'
+    start_time = Time.now
+    STDIN.echo = false
+    STDIN.raw!
+    begin
+      loop do
+        elapsed_time = Time.now - start_time
+        print "\r経過時間: #{elapsed_time.round(2)}秒"
+        break if IO.select([STDIN], [], [], 0.1)
+      end
+    ensure
+      STDIN.echo = true
+      STDIN.cooked!
+    end
+    puts "\nタスク計測を終了しました。"
+  end
+end
+TodoCLI.start(ARGV)
