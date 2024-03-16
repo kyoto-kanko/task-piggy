c091526deb06601f47899bed98872d232a5c24b8
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-03-16 17:43:00 +0000

    feat: oink機能

--- app/main.rb
+++ app/main.rb
@@ -1,26 +1,14 @@
 require 'thor'
 require 'csv'
-require 'io/console'
 
 class TodoCLI < Thor
   desc 'create TASK', '新しいタスクを作成'
-  TodoCLI.start(ARGV)
-  def oink
-    puts '今からタスクの計測を開始します。'
-    start_time = Time.now
-    STDIN.echo = false
-    STDIN.raw!
-    begin
-      loop do
-        elapsed_time = Time.now - start_time
-        print "\r経過時間: #{elapsed_time.round(2)}秒"
-        break if IO.select([STDIN], [], [], 0.1)
-      end
-    ensure
-      STDIN.echo = true
-      STDIN.cooked!
+  def oink(task)
+    time = Time.now.getlocal('+09:00').strftime('%Y-%m-%d %H:%M:%S')
+    puts "----------------------------Task registered.----------------------------\ntaskName: [#{task}], createdAt: [#{time}]\n------------------------------------------------------------------------"
+    CSV.open('todo.csv', 'a') do |csv|
     end
-    puts "\nタスク計測を終了しました。"
   end
 end
+
 TodoCLI.start(ARGV)
